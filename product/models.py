736b3016ed3024c56042141d48a79365fc59b20c
Ozan Ergön <ozanergon@gmail.com>
2020-04-08 15:12:46 +0300

    Alt kategori menü eklendi.

--- product/models.py
+++ product/models.py
@@ -3,8 +3,11 @@ from django.db import models
 # Create your models here.
 from django.utils.safestring import mark_safe
 from ckeditor_uploader.fields import RichTextUploadingField
+from mptt.fields import TreeForeignKey
+from mptt.models import MPTTModel
 
-class Category(models.Model):
+
+class Category(MPTTModel):
     STATUS = (
         ('True', 'Evet'),
         ('False', 'Hayır'),
@@ -15,12 +18,20 @@ class Category(models.Model):
     image = models.ImageField(blank=True, upload_to='images/')
     status = models.CharField(max_length=10, choices=STATUS)
     slug = models.SlugField()
-    parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
+    parent = TreeForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return self.title
+    class MPTTMeta:
+        order_insertion_by = ['title']
+
+    def __str__(self):  # __str__ method elaborated later in
+        full_path = [self.title]  # post.  use __unicode__ in place of
+        k = self.parent
+        while k is not None:
+            full_path.append(k.title)
+            k = k.parent
+        return ' / '.join(full_path[::-1])
 
     def image_tag(self):
         return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
