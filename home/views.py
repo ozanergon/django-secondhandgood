868796d199971523d797dae9b786a66c9073341d
Ozan Ergön <ozanergon@gmail.com>
2020-04-11 01:26:11 +0300

    Kategori ürünleri gösterildi.

--- home/views.py
+++ home/views.py
@@ -14,34 +14,48 @@ def index(request):
     count = 1
     context = {'setting': setting,
                'category': category,
-               'page':'home',
+               'page': 'home',
                'sliderdata': sliderdata}
     return render(request, 'index.html', context)
 
+
 def hakkimizda(request):
     setting = Setting.objects.get(pk=1)
-    context = {'setting': setting, 'page':'hakkimizda'}
+    context = {'setting': setting, 'page': 'hakkimizda'}
     return render(request, 'hakkimizda.html', context)
+
 
 def referanslar(request):
     setting = Setting.objects.get(pk=1)
     context = {'setting': setting}
     return render(request, 'referanslarimiz.html', context)
 
+
 def iletisim(request):
-    if request.method == 'POST': # form post edildiyse
+    if request.method == 'POST':  # form post edildiyse
         form = ContactFormu(request.POST)
         if form.is_valid():
-            data = ContactFormMessage() # model ile bağlantı kur
-            data.name = form.cleaned_data['name'] #formdan bilgiyi al
+            data = ContactFormMessage()  # model ile bağlantı kur
+            data.name = form.cleaned_data['name']  # formdan bilgiyi al
             data.email = form.cleaned_data['email']
             data.subject = form.cleaned_data['subject']
             data.message = form.cleaned_data['message']
             data.ip = request.META.get('REMOTE_ADDR')
-            data.save() # veritabanına kaydet
+            data.save()  # veritabanına kaydet
             messages.success(request, "Mesajınız başarı ile gönderilmiştir. Teşekkür Ederiz")
             return HttpResponseRedirect('/iletisim')
     setting = Setting.objects.get(pk=1)
     form = ContactFormu()
-    context = {'setting': setting, 'form':form}
+    context = {'setting': setting, 'form': form}
     return render(request, 'iletisim.html', context)
+
+
+def category_products(request, id, slug):
+    category = Category.objects.all()
+    categorydata = Category.objects.get(pk=id)
+    products = Product.objects.filter(category_id=id)
+    context = {'products': products,
+               'category': category,
+               'categorydata': categorydata
+               }
+    return render(request, 'products.html', context)
