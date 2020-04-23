643378804ce4932617b926f7d44e36f5564e21cd
Ozan Ergön <ozanergon@gmail.com>
2020-04-23 14:59:18 +0300

    Üye paneli ve profil bilgileri yapıldı

---
+++ user/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+from home.models import UserProfile
+from product.models import Category
+
+
+def index(request):
+    category = Category.objects.all()
+    current_user = request.user  # Access User Session information
+    profile = UserProfile.objects.get(pk=current_user.id)
+    context = {'category': category,
+               'profile': profile,
+               }
+    return render(request, 'user_profile.html', context)
